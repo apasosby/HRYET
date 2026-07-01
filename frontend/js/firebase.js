83ff68b71f642945f86430532cad1f3dabfece59
apasosby <henrrymoysesg@gmail.com>
2026-07-01 13:20:18 -0400

    Actualizar firebase.js

--- frontend/js/firebase.js
+++ frontend/js/firebase.js
@@ -1,23 +1,22 @@
-// Importar Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
 
-// Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyCoNoiTkueaqHxBwEUig3d9BX0taTjr3jc",
+  authDomain: "hryet-f2de3.firebaseapp.com",
+  projectId: "hryet-f2de3",
+  storageBucket: "hryet-f2de3.firebasestorage.app",
+  messagingSenderId: "839699465873",
+  appId: "1:839699465873:web:4cbb72d912c676b537a130",
+  measurementId: "G-8DK6LSF1TT"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exportar servicios
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+export { auth, db, analytics };
