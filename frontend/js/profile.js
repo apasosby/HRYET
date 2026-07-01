da8f64f6d3fbb8ba68a61e3a53d5bf429d8462ce
apasosby <henrrymoysesg@gmail.com>
2026-07-01 13:34:24 -0400

    Actualizar profile.js

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -1,1 +1,15 @@
-B
+// Escaneos de demostración
+const scans = 1240;
+
+// Actualizar contador
+document.getElementById("scanCount").textContent = scans;
+
+// Actualizar barra
+document.getElementById("progressBar").style.width =
+(scans / 10000) * 100 + "%";
+
+// Likes de demostración
+document.getElementById("likes").textContent = 368;
+
+// Escaneos de demostración
+document.getElementById("scans").textContent = scans;
