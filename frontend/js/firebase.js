04943435c246bcb88fd5e59e636aa98b8d8d37d5
apasosby <henrrymoysesg@gmail.com>
2026-07-01 13:02:17 -0400

    Actualizar firebase.js

--- frontend/js/firebase.js
+++ frontend/js/firebase.js
@@ -1,1 +1,47 @@
-Y
+// ==========================================
+// HRYET - FIREBASE CONFIG
+// ==========================================
+
+// IMPORTAR FIREBASE (VERSIÓN CDN)
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+
+
+// CONFIGURACIÓN FIREBASE
+// (LA COMPLETAREMOS CUANDO CREEMOS EL PROYECTO)
+
+const firebaseConfig = {
+
+apiKey: "",
+
+authDomain: "",
+
+projectId: "",
+
+storageBucket: "",
+
+messagingSenderId: "",
+
+appId: ""
+
+};
+
+
+// INICIALIZAR FIREBASE
+
+const app = initializeApp(firebaseConfig);
+
+
+// SERVICIOS
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
+
+export const storage = getStorage(app);
