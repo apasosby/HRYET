3dc350e457a49b27586f2dfecebb82697d0f2fd4
apasosby <henrrymoysesg@gmail.com>
2026-07-01 13:35:14 -0400

    Actualizar qr.js

--- frontend/js/qr.js
+++ frontend/js/qr.js
@@ -1,1 +1,29 @@
-B
+// =========================
+// HRYET QR SYSTEM
+// =========================
+
+// ID único del usuario
+const userId = "HRYET-0001";
+
+// URL del perfil
+const profileURL =
+window.location.origin +
+"/frontend/profile.html?id=" +
+userId;
+
+// Mostrar temporalmente el enlace
+const qrContainer = document.getElementById("qr");
+
+if(qrContainer){
+
+qrContainer.innerHTML = `
+<div class="qr-box">
+
+<h3>Tu Código QR</h3>
+
+<p>${profileURL}</p>
+
+</div>
+`;
+
+}
