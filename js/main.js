7c60d9c49c76df98a825ac8ea07442a33198f7ba
apasosby <henrrymoysesg@gmail.com>
2026-07-01 11:05:34 -0400

    Actualizar main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-console.log("HRYET iniciado correctamente 🚀");
+/*
+=========================================
+HRYET
+Main JavaScript
+Version 1.0
+=========================================
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("🚀 HRYET cargado correctamente");
+
+    const navbar = document.querySelector(".navbar");
+
+    window.addEventListener("scroll", () => {
+
+        if (window.scrollY > 50) {
+
+            navbar.style.background = "rgba(0,0,0,.90)";
+            navbar.style.boxShadow = "0 10px 30px rgba(0,0,0,.40)";
+
+        } else {
+
+            navbar.style.background = "rgba(0,0,0,.75)";
+            navbar.style.boxShadow = "none";
+
+        }
+
+    });
+
+});
